e6946c1e12d283e6bda324d0d70650e3f0e6d58a
yasitham <yasitham@millenniumit.com>
2016-11-20 23:38:54 +0530

    Add fileManager

--- src/main/java/distributed/computing/Main.java
+++ src/main/java/distributed/computing/Main.java
@@ -1,10 +1,11 @@
 package distributed.computing;
 
-import distributed.computing.domain.model.Operation;
+import distributed.computing.domain.model.*;
 import distributed.computing.listner.Listener;
 import distributed.computing.listner.TcpListener;
 import distributed.computing.listner.UdpListener;
 import distributed.computing.util.*;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,10 +22,8 @@ public class Main {
     public static void main(String[] args) {
         LOGGER.info("******************Starting client app!*******************");
         //TODO REG with BS
-      
-        String s = MessageUtils.createMsg(Operation.REG, Utils.getIP(), udpPort, "1234abcd");
-        System.out.println(s);
-      //  startListeners();
+    
+        startListeners();
     }
 
     private static void startListeners() {
---
+++ src/main/java/distributed/computing/domain/model/FileManager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributed.computing.domain.model;
+
+import java.util.*;
+
+/**
+ *
+ * @author yasitham
+ */
+public class FileManager {
+    
+    private final List<String> fileStorage = new ArrayList<>();
+    private Iterator<String> itr;
+    
+    public void addFile(String fileName){
+        fileStorage.add(fileName);
+    }
+    
+    public void removeFile(String fileName){
+        fileStorage.remove(fileName);
+    }
+    
+    public List<String> searchFile(String keyword){
+        List<String> searchResult = new ArrayList<>();
+        itr = fileStorage.iterator();
+         
+        for (String key : keyword.split(" ")){
+            while(itr.hasNext()){
+                String file = itr.next();
+                if(file.toLowerCase().contains(key.toLowerCase())){
+                    if(!searchResult.contains(file)){
+                        searchResult.add(file);
+                    }
+                }
+            }
+        }
+        return searchResult;
+    }
+}
