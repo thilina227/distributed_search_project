5a3ee838a2533f9b7e188da9e72ed4d13e2cdeb0
yasitham <yasitham@millenniumit.com>
2016-11-20 19:25:41 +0530

    Add REG, JOIN, SER msg creator

--- src/main/java/distributed/computing/Main.java
+++ src/main/java/distributed/computing/Main.java
@@ -1,8 +1,10 @@
 package distributed.computing;
 
+import distributed.computing.domain.model.Operation;
 import distributed.computing.listner.Listener;
 import distributed.computing.listner.TcpListener;
 import distributed.computing.listner.UdpListener;
+import distributed.computing.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,15 +14,17 @@ public class Main {
 
     //TODO get from config
     private static String bootstrapServerIp = "localhost";
-    private static int bootstrapServerPort = 9000;
+    private static int bootstrapServerPort = 5000;
 
-    private static int udpPort = 9001;//TODO get from args
+    private static int udpPort = 5001;//TODO get from args
 
     public static void main(String[] args) {
         LOGGER.info("******************Starting client app!*******************");
         //TODO REG with BS
-
-        startListeners();
+      
+        String s = MessageUtils.createMsg(Operation.REG, Utils.getIP(), udpPort, "1234abcd");
+        System.out.println(s);
+      //  startListeners();
     }
 
     private static void startListeners() {
--- src/main/java/distributed/computing/util/MessageUtils.java
+++ src/main/java/distributed/computing/util/MessageUtils.java
@@ -1,10 +1,33 @@
 package distributed.computing.util;
 
-
+import distributed.computing.domain.model.Operation;
 /**
  * Created by dev on 11/11/16.
  */
 public class MessageUtils {
 
     //TODO implement
+    public static String createMsg(Operation type, String ipAddress, int port, String name){
+        String msg = "";
+        int length;
+        String strlen;
+        String paddedlen;
+        
+        if(type == Operation.REG){
+            msg = String.format("REG %s %d %s", ipAddress, port, name); 
+        }
+        else if(type == Operation.JOIN){
+             msg = String.format("JOIN %s %d", ipAddress, port); 
+        }
+        else if(type == Operation.SER){
+            msg = String.format("SER %s %d %s", ipAddress, port, name); 
+        }
+        
+        length = msg.length() + 5;
+        strlen = Integer.toString(length);
+        paddedlen = "0000".substring(strlen.length()) + strlen;
+        
+        msg = paddedlen + " " + msg;
+        return msg;
+    }
 }
